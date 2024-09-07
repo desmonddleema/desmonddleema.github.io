8888377a9c4ea1c66e613523cace7fa09e19f9d9
gilfoyle-lite <desmond@avellinoconsulting.com>
2024-09-07 15:34:38 +0530

    Deployment fix #3

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   mode: "jit",
   theme: {
     extend: {
